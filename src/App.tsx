9a91cf42398fdc7f4ec7b27a4291c4b562490178
Nafiseh Ebrahimi <ebranafas@gmail.com>
2022-09-19 18:30:31 +0430

    insert components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { useState } from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import TextView from './components/TextView'
 
 function App() {
   return (
     <>
-      
+      <div className='w-[90] md:w-[600px] mx-auto'>
+        <Hero />
+        <Header />
+        <TextView />
+      </div>
     </>
   )
 }
