b35fe8200026a5ebb8e3dafc271ce97ad0ee93f0
Nafiseh Ebrahimi <ebranafas@gmail.com>
2022-09-21 12:02:13 +0430

    add helper file

--- src/components/TextView.tsx
+++ src/components/TextView.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { data } from "../data";
 
 function TextView() {
   return (
     <div className="mt-8">
-      <div className="w-full resize-none border-2 border-gray-400 bg-white rounded-lg min-h-[150px] my-4"></div>
+      <div className="py-3 px-5 w-full resize-none border-2 border-gray-400 bg-white rounded-lg min-h-[150px] my-4">
+        {
+          data[0].paragraph
+        }
+      </div>
       <button className="w-full py-3 shadow-box bg-dark text-white rounded-lg hover:opacity-60">
         کپی کن!
       </button>
