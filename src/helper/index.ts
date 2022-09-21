3e26ae92f976582f9f602a42f09b3cba85ffeb6a
Nafiseh Ebrahimi <ebranafas@gmail.com>
2022-09-21 12:09:10 +0430

    add getting shuffle array item function to helper

--- src/helper/index.ts
+++ src/helper/index.ts
@@ -1,3 +1,7 @@
 export function CopyText(value: string) {
     navigator.clipboard.writeText(value);
 }
+
+export function ShuffleArray(arr: string[]) {
+    return arr.length && arr[Math.floor(Math.random() * arr.length)];
+}
