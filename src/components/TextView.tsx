a52b6e9887c10d2824ff9114e1a26f05054a714c
Nafiseh Ebrahimi <ebranafas@gmail.com>
2022-09-21 13:29:50 +0430

    Handled get text and put it to text view by optoins

--- src/components/TextView.tsx
+++ src/components/TextView.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { TextContext } from "../context/TextContext";
 import { data } from "../data";
 
 function TextView() {
+  const useTextContext = useContext(TextContext);
+
   return (
     <div className="mt-8">
       <div className="py-3 px-5 w-full resize-none border-2 border-gray-400 bg-white rounded-lg min-h-[150px] my-4">
-        {
-          data[0].paragraph
-        }
+        {data[0][useTextContext.textState]}
       </div>
       <button className="w-full py-3 shadow-box bg-dark text-white rounded-lg hover:opacity-60">
         کپی کن!
