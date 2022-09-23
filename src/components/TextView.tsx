140f0bfd2929df71dae6958ae715ba57942d7d11
Nafiseh Ebrahimi <ebranafas@gmail.com>
2022-09-23 10:38:31 +0330

    handle shuffle array data text

--- src/components/TextView.tsx
+++ src/components/TextView.tsx
@@ -11,9 +11,9 @@ function TextView() {
   return (
     <div className="mt-8">
       <div className="py-3 px-5 w-full resize-none border-2 border-gray-400 bg-white rounded-lg min-h-[150px] my-4">
-        {data[0][useTextContext.textState]}
+        {useTextContext.textData[useTextContext.textState]}
       </div>
-      <button onClick={() => CopyText(data[0][useTextContext.textState])} className="w-full py-3 shadow-box bg-dark text-white rounded-lg hover:opacity-60">
+      <button onClick={() => CopyText(useTextContext.textData[useTextContext.textState])} className="w-full py-3 shadow-box bg-dark text-white rounded-lg hover:opacity-60">
         کپی کن!
       </button>
     </div>
