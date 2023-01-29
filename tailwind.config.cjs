9b6a4c30bd0b8d83008f97a55ea3d6af3c98f2f7
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-01-29 15:29:23 +0330

    config: add dark mode variable to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,7 +12,16 @@ module.exports = {
       },
       boxShadow: {
         'box': "0 3px 10px #2c3e5030"
-      }
+      },
+      backgroundColor: {
+        primary: 'var(--color-bg-primary)',
+        secondary: 'var(--color-bg-secondary)',
+      },
+      textColor: {
+        accent: 'var(--color-border-accent)',
+        primary: 'var(--color-text-primary)',
+        secondary: 'var(--color-text-secondary)',
+      },
     },
   },
   plugins: [],
