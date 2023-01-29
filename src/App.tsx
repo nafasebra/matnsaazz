eadebd17910a253aa99b90c4377ad4e0cda6ae9c
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-01-29 16:53:06 +0330

    fix and chore: fix root variable and create ToggleButton

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import TextView from "./components/TextView";
+import ToggleButton from "./components/ToggleButton";
 import { TextProvider } from "./context/TextContext";
 import ThemeProvider from "./context/ThemeContext";
 
 function App() {
   return (
     <>
-      <div className="w-[90%] md:w-[600px] mx-auto">
+      <div className="w-[90%] md:w-[600px] mx-auto relative">
         <ThemeProvider initialTheme="light">
+          <ToggleButton />
           <Hero />
           <TextProvider>
             <Header />
