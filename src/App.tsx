b72d3ef1a51c5ccba55a312c902d24a9a64a5665
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-01-29 15:57:07 +0330

    feat: wrapp all component with themeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import TextView from "./components/TextView";
 import { TextProvider } from "./context/TextContext";
+import ThemeProvider from "./context/ThemeContext";
 
 function App() {
   return (
     <>
       <div className="w-[90%] md:w-[600px] mx-auto">
-        <Hero />
-        <TextProvider>
-          <Header />
-          <TextView />
-        </TextProvider>
-        <Footer />
+        <ThemeProvider initialTheme="light">
+          <Hero />
+          <TextProvider>
+            <Header />
+            <TextView />
+          </TextProvider>
+          <Footer />
+        </ThemeProvider>
       </div>
     </>
   );
