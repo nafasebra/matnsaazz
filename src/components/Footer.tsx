33141573e50b933652a20399ba06cb850e58bcfe
unknown <ebranafas@gmail.com>
2023-01-30 13:13:27 +0330

    change hero and footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className='text-center mt-10 py-4 px-2 text-primary'>
-        <p className='font-bold text-sm opacity-80'> ساخته شده با ❤ توسط <a href="https://github.com/nafasebra" className='text-accent'>نفس ابراهیمی</a> </p>
+        <p className='font-bold text-sm opacity-80'> ساخته شده با ❤ </p>
     </footer>
   )
 }
