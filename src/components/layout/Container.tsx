4177ba8e52277899190bcaa3d81c2f49637b1bd7
unknown <051.sys113@gmail.com>
2024-03-16 23:25:44 +0330

    fix: fix transition for change background in first render

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -8,10 +8,8 @@ function Container(props: ContainerProps) {
   const { children } = props;
 
   return (
-    <div className=" bg-secondary transition-none mx-auto px-4 flex justify-center items-center">
-      <div className="w-[90%] md:w-[600px] relative min-h-screen flex flex-col">
+    <div className="mx-auto px-4 justify-center items-center w-[90%] md:w-[600px] relative min-h-screen flex flex-col">
         {children}
-      </div>
     </div>
   );
 }
